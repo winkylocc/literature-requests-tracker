3a30268da4b87f5ab44823c8179f8c32fe993f57
Neil McCauley <bgswansonlocc.com>
2025-07-07 23:52:52 -0500

    enable pwa offline support with service worker

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,36 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt'],
+      includeAssets: ['favicon/favicon.ico',
+        'favicon/apple-touch-icon.png',
+        'favicon/favicon-96x96.png',
+        'favicon/favicon.svg'],
       manifest: {
-        name: 'Literature Tracker',
-        short_name: 'LitTracker',
-        description: 'Track literature requests offline',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        scope: '/',
+        name: 'Literature Requests Tracker',
+        short_name: 'LitReqs  ',
         start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#3f51b5',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: 'public/favicon/web-app-manifest-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: 'public/favicon/web-app-manifest-512x512.png',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/png'
           }
         ]
       }
     })
   ]
-});
+})
